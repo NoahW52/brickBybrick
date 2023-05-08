950453c754dca6031bcd41a57ecc0a523ab168bc
Noah Wright <nwright5252@gmail.com>
2023-05-07 23:51:05 -0500

    getting the boiler plate done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Reducer from './store/Reducer'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+const store = createStore(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
